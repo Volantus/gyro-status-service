8abd525abe42ca64ee42b0d75133aa35c2e301e3
Marius Meissner <info@marius-meissner.net>
2017-01-27 23:52:38 +0100

    Added GyroStatusRepository

--- src/GyroStatus/GyroStatusRepository.php
+++ src/GyroStatus/GyroStatusRepository.php
@@ -1,6 +1,9 @@
 <?php
 namespace Volante\SkyBukkit\GyroStatusService\Src\GyroStatus;
 
+use Symfony\Component\Console\Output\OutputInterface;
+use Volante\SkyBukkit\Common\Src\General\CLI\OutputOperations;
+
 /**
  * Class GyroStatusRepository
  *
@@ -8,5 +11,54 @@ namespace Volante\SkyBukkit\GyroStatusService\Src\GyroStatus;
  */
 class GyroStatusRepository
 {
+    use OutputOperations;
 
+    /**
+     * @var Socket
+     */
+    private $socket;
+
+    /**
+     * GyroStatusRepository constructor.
+     *
+     * @param OutputInterface $output
+     * @param Socket          $socket
+     */
+    public function __construct(OutputInterface $output, Socket $socket = null)
+    {
+        $this->socket = $socket ?: new Socket('127.0.0.1', 5555);
+        $this->output = $output;
+    }
+
+    /**
+     * @return GyroStatus
+     */
+    public function getLatestStatus() : GyroStatus
+    {
+        $data = $this->socket->listen();
+
+        $segments = explode(',', $data);
+        if (count($segments) < 2) {
+            throw new \InvalidArgumentException('Invalid low level gyro message: Missing message separator');
+        }
+        array_pop($segments);
+
+        if (count($segments) > 1) {
+            $this->writeErrorLine('GyroStatusRepository', 'Hanging back! Received ' . count($segments) . ' segments from the socket buffer. Will only return the latest status.');
+        }
+
+        $values = explode(' ', end($segments));
+
+        if (count($values) != 3) {
+            throw new \InvalidArgumentException('Invalid low level gyro message: Value Count is not correct!');
+        }
+
+        foreach ($values as $i => $value) {
+            if (!is_numeric($value)) {
+                throw new \InvalidArgumentException('Invalid low level gyro message: Value ' . $i . ' is not numeric!');
+            }
+        }
+
+        return new GyroStatus((float) $values[0],(float) $values[1],(float) $values[2]);
+    }
 }
---
+++ tests/GyroStatus/GyroStatusRepositoryTest.php
@@ -0,0 +1,129 @@
+<?php
+namespace Volante\SkyBukkit\GyroStatusService\Tests\GyroStatus;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use Volante\SkyBukkit\GyroStatusService\Src\GyroStatus\GyroStatus;
+use Volante\SkyBukkit\GyroStatusService\Src\GyroStatus\GyroStatusRepository;
+use Volante\SkyBukkit\GyroStatusService\Src\GyroStatus\Socket;
+
+/**
+ * Class GyroStatusRepositoryTest
+ *
+ * @package Volante\SkyBukkit\GyroStatusService\Tests\GyroStatus
+ */
+class GyroStatusRepositoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var GyroStatusRepository
+     */
+    private $repository;
+
+    /**
+     * @var OutputInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $outputInterface;
+
+    /**
+     * @var Socket|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $socket;
+
+    protected function setUp()
+    {
+        $this->socket = $this->getMockBuilder(Socket::class)->disableOriginalConstructor()->getMock();
+        $this->outputInterface = $this->getMockBuilder(OutputInterface::class)->disableOriginalConstructor()->getMock();
+
+        $this->repository = new GyroStatusRepository($this->outputInterface, $this->socket);
+    }
+
+    public function test_getLatestStatus_singleSegment_correct()
+    {
+        $this->outputInterface->expects(self::never())->method('writeln');
+        $this->outputInterface->expects(self::never())->method('write');
+        $this->socket->expects(self::once())->method('listen')
+            ->willReturn('1.1 2.2 3.3,abc');
+        $result = $this->repository->getLatestStatus();
+
+        self::assertInstanceOf(GyroStatus::class, $result);
+        self::assertEquals(1.1, $result->getYaw());
+        self::assertEquals(2.2, $result->getRoll());
+        self::assertEquals(3.3, $result->getPitch());
+    }
+
+    public function test_getLatestStatus_multipleSegments_latestSegmentOnlyAndLogMessage()
+    {
+        $this->outputInterface->expects(self::once())->method('writeln')
+            ->will(self::returnCallback(function ($message) {
+                self::assertStringEndsWith('<error>Hanging back! Received 2 segments from the socket buffer. Will only return the latest status.</error>', $message);
+            }));
+
+        $this->socket->expects(self::once())->method('listen')
+            ->willReturn('0 0 0,1.1 2.2 3.3,abc');
+
+        $result = $this->repository->getLatestStatus();
+        self::assertInstanceOf(GyroStatus::class, $result);
+        self::assertEquals(1.1, $result->getYaw());
+        self::assertEquals(2.2, $result->getRoll());
+        self::assertEquals(3.3, $result->getPitch());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Invalid low level gyro message: Missing message separator
+     */
+    public function test_getLatestStatus_invalidData_missingSeparator()
+    {
+        $this->socket->expects(self::once())->method('listen')
+            ->willReturn('');
+
+        $this->repository->getLatestStatus();
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Invalid low level gyro message: Value Count is not correct!
+     */
+    public function test_getLatestStatus_invalidData_valueMissing()
+    {
+        $this->socket->expects(self::once())->method('listen')
+            ->willReturn('1.1 2.2,abc');
+
+        $this->repository->getLatestStatus();
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Invalid low level gyro message: Value 0 is not numeric!
+     */
+    public function test_getLatestStatus_invalidData_firstValueNotFloat()
+    {
+        $this->socket->expects(self::once())->method('listen')
+            ->willReturn('abc 1 2,');
+
+        $this->repository->getLatestStatus();
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Invalid low level gyro message: Value 1 is not numeric!
+     */
+    public function test_getLatestStatus_invalidData_secondValueNotFloat()
+    {
+        $this->socket->expects(self::once())->method('listen')
+            ->willReturn('1 abc 2,');
+
+        $this->repository->getLatestStatus();
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Invalid low level gyro message: Value 2 is not numeric!
+     */
+    public function test_getLatestStatus_invalidData_thirdValueNotFloat()
+    {
+        $this->socket->expects(self::once())->method('listen')
+            ->willReturn('1 2 abc,');
+
+        $this->repository->getLatestStatus();
+    }
+}
