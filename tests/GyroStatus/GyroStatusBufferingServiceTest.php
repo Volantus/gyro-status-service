2e6161a24d2e26300f228ae10c70d56e0d5c2374
Marius Meissner <info@marius-meissner.net>
2017-01-28 23:24:28 +0100

    Added broadcasting to buffering service!

--- tests/GyroStatus/GyroStatusBufferingServiceTest.php
+++ tests/GyroStatus/GyroStatusBufferingServiceTest.php
@@ -2,8 +2,13 @@
 namespace Volante\SkyBukkit\GyroStatusService\Tests\GyroStatus;
 
 use React\EventLoop\LoopInterface;
+use Ratchet\ConnectionInterface;
+use Volante\SkyBukkit\Common\Src\General\Role\ClientRole;
 use Volante\SkyBukkit\Common\Src\Server\Messaging\MessageServerService;
+use Volante\SkyBukkit\Common\Src\Server\Network\Client;
+use Volante\SkyBukkit\Common\Tests\Server\General\DummyConnection;
 use Volante\SkyBukkit\Common\Tests\Server\Messaging\MessageServerServiceTest;
+use Volante\SkyBukkit\GyroStatusService\Src\GyroStatus\GyroStatus;
 use Volante\SkyBukkit\GyroStatusService\Src\GyroStatus\GyroStatusBufferingService;
 use Volante\SkyBukkit\GyroStatusService\Src\GyroStatus\GyroStatusRepository;
 
@@ -14,6 +19,11 @@ use Volante\SkyBukkit\GyroStatusService\Src\GyroStatus\GyroStatusRepository;
  */
 class GyroStatusBufferingServiceTest extends MessageServerServiceTest
 {
+    /**
+     * @var GyroStatusBufferingService
+     */
+    protected $messageServerService;
+
     /**
      * @var GyroStatusRepository|\PHPUnit_Framework_MockObject_MockObject
      */
@@ -37,17 +47,16 @@ class GyroStatusBufferingServiceTest extends MessageServerServiceTest
         return new GyroStatusBufferingService($this->dummyOutput, $this->loopInterface, $this->messageService, $this->clientFactory, $this->repository);
     }
 
-//    public function test_sendPosition_positionRefreshedAndSend()
-//    {
-//        $connection = $this->getMockBuilder(ConnectionInterface::class)->disableOriginalConstructor()->getMock();
-//        $connection->expects(self::once())->method('send')->with('dsfdsfdsfsdf');
-//        /** @var ConnectionInterface $connection */
-//        $this->clientFactory->method('get')->willReturn(new Client(1, $connection, ClientRole::STATUS_BROKER));
-//
-//        $this->geoPositionRepository->expects(self::once())->method('refresh');
-//        $this->geoPositionRepository->expects(self::once())->method('getCurrentPosition')->willReturn(new GeoPosition(1, 2, 3));
-//
-//        $this->service->newClient(new DummyConnection());
-//        $this->service->sendPosition();
-//    }
+    public function test_sendStatus_broadcastedCorrectly()
+    {
+        $connection = $this->getMockBuilder(ConnectionInterface::class)->disableOriginalConstructor()->getMock();
+        $connection->expects(self::once())->method('send')->with('{"type":"gyroStatus","title":"Gyro Status","data":{"yaw":1,"pitch":3,"roll":2}}');
+        /** @var ConnectionInterface $connection */
+        $this->clientFactory->method('get')->willReturn(new Client(1, $connection, ClientRole::STATUS_BROKER));
+
+        $this->repository->expects(self::once())->method('getLatestStatus')->willReturn(new GyroStatus(1, 2, 3));
+
+        $this->messageServerService->newClient(new DummyConnection());
+        $this->messageServerService->sendStatus();
+    }
 }
