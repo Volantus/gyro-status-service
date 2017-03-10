d46298bdb6d34d178f934935ee48625fbd7923ca
Marius Meissner <info@marius-meissner.net>
2017-03-10 20:55:21 +0100

    Added zero gyro level

--- src/Commands/ServerCommand.php
+++ src/Commands/ServerCommand.php
@@ -8,10 +8,12 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Volante\SkyBukkit\Common\Src\General\GyroStatus\GyroStatus;
 use Volante\SkyBukkit\GyroStatusService\Src\Controller;
 use React\EventLoop\Factory as LoopFactory;
 use React\Socket\Server as Reactor;
 use Volante\SkyBukkit\GyroStatusService\Src\GyroStatus\GyroStatusBufferingService;
+use Volante\SkyBukkit\GyroStatusService\Src\GyroStatus\GyroStatusRepository;
 
 /**
  * Class ServerCommand
@@ -25,6 +27,10 @@ class ServerCommand extends Command
         $this->setDescription('Runs the gyro status service');
 
         $this->addOption('port', 'p', InputArgument::OPTIONAL, 'Port of the webSocket', 5002);
+
+        $this->addOption('zeroYaw', 'y', InputArgument::OPTIONAL, 'Zero yaw level', 0);
+        $this->addOption('zeroPitch', 'p', InputArgument::OPTIONAL, 'Zero pitch level', 0);
+        $this->addOption('zeroRoll', 'r', InputArgument::OPTIONAL, 'Zero roll level', 0);
     }
 
     /**
@@ -38,7 +44,8 @@ class ServerCommand extends Command
         $socket = new Reactor($loop);
         $socket->listen($input->getOption('port'), '127.0.0.1');
 
-        $service = new GyroStatusBufferingService($output, $loop);
+        $repository = new GyroStatusRepository($output, new GyroStatus($input->getOption('zeroYaw'), $input->getOption('zeroRoll'), $input->getOption('zeroPitch')));
+        $service = new GyroStatusBufferingService($output, $loop, null, null, $repository);
         $controller = new Controller($output, $service);
 
         $server = new IoServer(new HttpServer(new WsServer($controller)), $socket, $loop);
